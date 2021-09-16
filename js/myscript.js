51fed90dd794dacd9a2661321935b6314dfc08f8
daviddpi <piscopodavid97@gmail.com>
2021-09-16 15:39:18 +0200

    esercizio completato

--- js/myscript.js
+++ js/myscript.js
@@ -2,10 +2,30 @@ let main = new Vue({
 
     el: "#root",
     data: {
-        listaSpesa: ["uova", "latte", "farina", "acqua", "sugo", "patate", "peperoni", "sofficini"],
+        listItem: ["uova", "latte", "farina", "acqua", "sugo", "patate", "peperoni", "sofficini"],
         addItem: "",
+        errorInsert: false,
     },
     methods: {
 
+        //quando clicco sulla X elimino un oggetto dalla lista
+        deleteItem(index){
+            this.listItem.splice(index, 1);
+        },
+
+        //aggiungo un elemento alla lista della spesa
+        addItemFunction(){
+            this.errorInsert = false;
+            if(this.listItem.includes(this.addItem)){
+                this.errorInsert = true;
+            }
+
+            if( (this.addItem.trim('').length > 0) && !(this.listItem.includes(this.addItem)) ){
+                this.listItem.push(this.addItem);
+            }
+            
+            this.addItem = "";
+            
+        }
     }
 });
