13e0930786a0e65617e3b81afddc42ac6a0e81d1
daviddpi <piscopodavid97@gmail.com>
2021-09-16 15:08:07 +0200

    completata strutta html inizio js

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+let main = new Vue({
+
+    el: "#root",
+    data: {
+        listaSpesa: ["uova", "latte", "farina", "acqua", "sugo", "patate", "peperoni", "sofficini"],
+        addItem: "",
+    },
+    methods: {
+
+    }
+});
